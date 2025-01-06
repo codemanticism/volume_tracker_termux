db2fff4d3607e7516ba1a5f139f6a7b1ff28310d
gabib <gabibkaminski@gmail.com>
2025-01-06 11:51:44 -0300

    start

---
+++ main.py
@@ -0,0 +1,34 @@
+import math
+import time
+import pyaudio
+p = pyaudio.PyAudio()
+print(p)
+def barfrom(value, start, end, long):
+    length = (value - start) / (end - start)
+    length = length * long
+    bar = ""
+    for i in range(int(length)):
+        bar += "="
+    print("[", bar, "]")
+def tointeger(data, position, bytecount):
+    total = 0
+    for i in range(bytecount):
+        total = total * 256
+        total += data[position + i]
+    return total
+def volume(audio, samplebytes):
+    total = 0
+    length = math.floor(len(audio) / samplebytes)
+    for i in range(length):
+        total += tointeger(audio, i * samplebytes, samplebytes)
+    return total / length
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+stream = p.open(format=FORMAT, channels = 0, rate = RATE, input = True, frames_per_buffer = CHUNK, input_device_index = 0)
+try:
+    data = stream.read(30)
+    print(data)
+except KeyboardInterrupt:
+    print("Stopping...")
---
+++ pip.sh
@@ -0,0 +1,4 @@
+sudo apt install portaudio19-dev python3-dev
+sudo apt install python3-pip
+pip install pyaudio
+pip install numpy
